dcce3ce393b8d56b8abb474f672b8b7a51e3af9f
ravinamotwani11 <ravinamotwani11@gmail.com>
2023-07-23 22:02:47 +0530

    Code adjustment

--- library.management/src/main/java/com/technogise/library/management/service/LibraryService.java
+++ library.management/src/main/java/com/technogise/library/management/service/LibraryService.java
@@ -1,5 +1,20 @@
 package com.technogise.library.management.service;
 
-public class LibraryService {
+import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+import com.technogise.library.management.entity.Book;
+import com.technogise.library.management.entity.BorrowedBook;
+import com.technogise.library.management.exception.LibraryException;
+
+@Service
+public interface LibraryService {
+
+	public Book addBook(Book book);
+	public List<Book> getAllBooks();
+	public boolean borrowBook(String userId, String bookTitle) throws LibraryException;
+	public boolean returnBook(String userId, String bookTitle) throws LibraryException;
+	public List<BorrowedBook> getBorrowedBooksByUser(String userId);
+	
 }
